f8d8d5b31ead4686ba84e033e3035fd0f1e057f5
Osama Younus <osamayounus541@gmail.com>
2023-04-20 16:11:34 +0500

    frist vid

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello Express Js")
+
+
+
+const express = require('express');
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+        res.send("helllo mamasitta");
+})
+
+app.listen(3000,()=>{
+    console.log("server listening on 3000");
+})
