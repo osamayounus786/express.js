a0e7e269bfefde0fba04db7785403064451a3402
Osama Younus <osamayounus541@gmail.com>
2023-04-28 18:32:25 +0500

    joined path

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// console.log(__dirname);
+// console.log(path.join());
+// console.log(app.use(express.static()));
+
+// console.log(path.join(__dirname,'../public'));
+
+// const staticPath = path.join(__dirname, '../public');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.use(express.static(staticPath));
+
+app.get('/home',(req,res)=>{
+        res.send("helllo mamasitta");
+})
+
+app.listen(3000,()=>{
+    console.log("server listening on 3000");
+})
+
+
+// express js to be continue 
